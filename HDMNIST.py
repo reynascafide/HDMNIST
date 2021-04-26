8798d0165cd8034fa1e41319b29d482299ffd4c0
reynascafide <82096685+reynascafide@users.noreply.github.com>
2021-04-26 15:15:34 -0400

    Update HDMNIST.py

--- HDMNIST.py
+++ HDMNIST.py
@@ -139,12 +139,12 @@ def main(mode):
             position_table = lookup_generate(eachdim, datatype, imgsize*imgsize)
             grayscale_table = lookup_generate(eachdim, datatype, maxval)        
             am = np.zeros((n_class, eachdim), dtype = np.int16)
-            am_classHVs = np.zeros([10, 1], dtype = np.float32)
+            am_classHVs = np.zeros([10, 1], dtype = np.int16)
             am = train(am, X_train, Y_train, position_table, grayscale_table, eachdim)
             for epoch in range(retraining_epoch):
                 print('Retraining epoch: ' + str(epoch))
                 am = retrain(am, X_train[:train_size], Y_train[:train_size], position_table, grayscale_table, eachdim)
-                am_classHVs = am.sum(axis=1)
+                am_classHVs = (am.sum(axis=1)).astype(np.uint16)
                 with open('output.mif', 'w') as mif_file:
                     mif.dump(am_classHVs, mif_file, address_radix='HEX', data_radix='DEC')
             test(am, X_test[:test_size], Y_test[:test_size], position_table, grayscale_table, eachdim)
