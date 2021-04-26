b0527032c574939a045c8427bb0ccd9702dd34d3
reynascafide <82096685+reynascafide@users.noreply.github.com>
2021-04-26 15:07:32 -0400

    Update HDMNIST.py

--- HDMNIST.py
+++ HDMNIST.py
@@ -139,7 +139,7 @@ def main(mode):
             position_table = lookup_generate(eachdim, datatype, imgsize*imgsize)
             grayscale_table = lookup_generate(eachdim, datatype, maxval)        
             am = np.zeros((n_class, eachdim), dtype = np.int16)
-            am_classHVs = np.zeros([10 1], dtype = np.float32)
+            am_classHVs = np.zeros([10, 1], dtype = np.float32)
             am = train(am, X_train, Y_train, position_table, grayscale_table, eachdim)
             for epoch in range(retraining_epoch):
                 print('Retraining epoch: ' + str(epoch))
